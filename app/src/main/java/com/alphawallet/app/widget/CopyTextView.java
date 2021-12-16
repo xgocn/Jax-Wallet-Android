ef815ae38675d66e7b8379bfb2957943614b2d64
gitdevstar <56650395+gitdevstar@users.noreply.github.com>
2021-12-16 04:20:20 -0500

    fix Copytextview attr

--- app/src/main/java/com/alphawallet/app/widget/CopyTextView.java
+++ app/src/main/java/com/alphawallet/app/widget/CopyTextView.java
@@ -28,6 +28,8 @@ public class CopyTextView extends LinearLayout {
     private int textResId;
     private int textColor;
     private int gravity;
+    private int background;
+    private int height;
     private boolean showToast;
     private boolean boldFont;
     private boolean removePadding;
@@ -56,6 +58,8 @@ public class CopyTextView extends LinearLayout {
             textResId = a.getResourceId(R.styleable.CopyTextView_text, R.string.action_add_wallet);
             textColor = a.getColor(R.styleable.CopyTextView_textColour, context.getColor(R.color.text_black));
             gravity = a.getInt(R.styleable.CopyTextView_android_gravity, Gravity.NO_GRAVITY);
+            background = a.getResourceId(R.styleable.CopyTextView_backgroundView, R.drawable.background_round_border);
+            height = a.getResourceId(R.styleable.CopyTextView_minHeightView, 0);
             showToast = a.getBoolean(R.styleable.CopyTextView_showToast, true);
             boldFont = a.getBoolean(R.styleable.CopyTextView_bold, false);
             removePadding = a.getBoolean(R.styleable.CopyTextView_removePadding, false);
@@ -72,6 +76,8 @@ public class CopyTextView extends LinearLayout {
         text.setText(textResId);
         text.setTextColor(textColor);
         text.setGravity(gravity);
+        layout.setBackgroundResource(background);
+        layout.setMinimumHeight(height);
 
         LayoutParams layoutParams = (LayoutParams) text.getLayoutParams();
         layoutParams.rightMargin = (int) marginRight;

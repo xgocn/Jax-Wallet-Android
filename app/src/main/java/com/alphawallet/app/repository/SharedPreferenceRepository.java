76eb0bc09aa0d297269203a923bbc840ede32524
gitdevstar <56650395+gitdevstar@users.noreply.github.com>
2022-01-26 07:52:58 -0500

    set BSC test net as default

--- app/src/main/java/com/alphawallet/app/repository/SharedPreferenceRepository.java
+++ app/src/main/java/com/alphawallet/app/repository/SharedPreferenceRepository.java
@@ -182,7 +182,7 @@ public class SharedPreferenceRepository implements PreferenceRepositoryType {
 
     @Override
     public boolean isActiveMainnet() {
-        return pref.getBoolean(ACTIVE_MAINNET, true);
+        return pref.getBoolean(ACTIVE_MAINNET, false);
     }
 
     @Override
